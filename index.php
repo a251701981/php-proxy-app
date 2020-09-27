9dc86f6cd950e4ef126209f3d4294418d1c5d29d
a251701981 <251701981@qq.com>
2020-09-27 10:55:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ error_reporting(E_ALL);
 define('PROXY_START', microtime(true));
 
 require("vendor/autoload.php");
+echo date('H:i:s');exit;
 use Proxy\Config;
 use Proxy\Http\Request;
 use Proxy\Proxy;
